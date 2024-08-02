7e20af05aa3fe395d30838c9719fd273276ca2a5
Ayush Suryan <suryanayush@gmail.com>
2024-08-02 17:45:34 +0530

    added auth

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -4,14 +4,17 @@ const { userModel } = require("../db");
 const { balanceModel } = require("../db");
 const zod = require("zod");
 const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../config");
 
-const signinBody = zod.object({
+const userInputValidation = zod.object({
   username: zod.string().email(),
   password: zod.string(),
 });
 
+//Signin
+
 router.post("/signin", async (req, res) => {
-  const { success } = signinBody.safeParse(req.body);
+  const { success } = userInputValidation.safeParse(req.body);
   if (!success) {
     return res.status(411).json({ message: "Invalid Input" });
   }
@@ -21,30 +24,52 @@ router.post("/signin", async (req, res) => {
     password: req.body.password,
   });
 
+  if (!user) {
+    return res.status(401).json({ message: "User Not Found" });
+  }
+
   if (user) {
     const token = jwt.sign(
       {
         userId: user._id,
       },
-      "secretKey",
+      JWT_SECRET,
       { expiresIn: "30m" }
     );
     res.json({
       token: token,
     });
     return;
-  } else {
-    const user = await userModel.create({
-      username: req.body.username,
-      password: req.body.password,
-    });
-    const userId = user._id;
-
-    await balanceModel.create({
-      userId,
-      balance: Math.floor(1 + Math.random() * 10000),
-    });
   }
+});
+
+//Signup
+
+router.post("/signup", async (req, res) => {
+  const { success } = userInputValidation.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({ message: "Invalid Input" });
+  }
+
+  const existingUser = await userModel.findOne({
+    username: req.body.username,
+  });
+
+  if (existingUser) {
+    return res.status(401).json({ message: "Email already in Usage" });
+  }
+
+  const user = await userModel.create({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  const userId = user._id;
+
+  await balanceModel.create({
+    id: userId,
+    balance: 1000,
+  });
+  res.status(200).json({ message: "Account created successfully." });
 });
 
 module.exports = router;
