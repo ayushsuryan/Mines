493e2d2af4a27d7d7f7ec280f19ca07ddd42ffd9
Ayush Suryan <suryanayush@gmail.com>
2024-08-02 22:23:59 +0530

    Done with open and start

--- backend/routes/betRouter.js
+++ backend/routes/betRouter.js
@@ -3,18 +3,51 @@ const router = express.Router();
 const { resultModel } = require("../db");
 const { authMiddleware } = require("../middlewares/authMiddleware");
 
-router.put("/mines/start", authMiddleware, async (req, res) => {
-  // const objectId = await resultModel.findOne({ id:  });
+function generateRandomArray(numOnes) {
+  if (numOnes > 25 || numOnes < 0) {
+    throw new Error("Input must be between 0 and 25");
+  }
 
-  await resultModel.updateOne({ id: req.userId }, { $set: { result: [1] } });
+  // Create an array with 25 zeros
+  let arr = Array(25).fill(0);
+
+  // Add numOnes 1s at random positions
+  let onesAdded = 0;
+  while (onesAdded < numOnes) {
+    let randomIndex = Math.floor(Math.random() * 25);
+    if (arr[randomIndex] === 0) {
+      arr[randomIndex] = 1;
+      onesAdded++;
+    }
+  }
+
+  return arr;
+}
+
+router.put("/mines/start", authMiddleware, async (req, res) => {
+  // http://localhost:3000/v1/bet/mines/start?mines=4
+  const noOfMines = req.query.mines;
+  await resultModel.updateOne(
+    { id: req.userId },
+    { $set: { result: generateRandomArray(noOfMines) } }
+  );
 
   res.status(200).json({
     message: "Game Started",
   });
 });
 
-router.post("/mines/open", (req, res) => {
-  console.log(req);
+router.get("/mines/open", async (req, res) => {
+  const tileSelected = req.body.mineId;
+  const resultInDb = await resultModel.aggregate([
+    {
+      $project: {
+        nthElement: { $arrayElemAt: ["$result", tileSelected - 1] },
+      },
+    },
+  ]);
+  const outcome = 1 == resultInDb[0].nthElement ? false : true;
+  res.send(outcome);
 });
 
 module.exports = router;
